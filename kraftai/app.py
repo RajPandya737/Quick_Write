f3d339ca9a66d83a242d20eb8f91ce66287fa6c7
Raj Pandya <rajpandya737@gmail.com>
2023-09-09 15:49:50 -0400

    init flask backend and rest api

---
+++ kraftai/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify, render_template
+from scan_text import ocr, whiteify
+import random
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')  
+
+
+
+@app.route('/save-image', methods=['POST'])
+def save_image():
+    try:
+        data = request.json
+        image_data = data.get('image')
+
+        if image_data:
+
+            image_data = image_data.replace('data:image/png;base64,', '')
+
+            image_path = 'static/drawings/test.png'
+
+            with open(image_path, 'wb') as f:
+                import base64
+                f.write(base64.b64decode(image_data))
+            whiteify(image_path)
+            ocr_text = ocr(image_path)
+            print(ocr_text)
+            return jsonify({"ocr": ocr_text}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000) 
