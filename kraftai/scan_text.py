0be4df39f02b91a6a359cc82a0585924add559b5
Raj Pandya <rajpandya737@gmail.com>
2023-09-09 15:46:46 -0400

    add whitfy function

--- kraftai/scan_text.py
+++ kraftai/scan_text.py
@@ -8,3 +8,17 @@ def ocr(path):
     ).replace("\n\x0c", "")
 
 
+def whiteify(path):
+    try:
+        image = Image.open(path)
+        width, height = image.size
+        new_image = Image.new("RGBA", (width, height), (255, 255, 255, 255))
+        new_image.paste(image, (0, 0), image)
+        new_image.save(path)
+        image.close()
+        new_image.close()
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
+print(ocr("static/drawings/main.png"))
